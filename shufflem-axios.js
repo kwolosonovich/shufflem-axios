8df06b18b6877f2c5fba2f80c15b29f50245913c
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-08-16 22:35:41 -0700

    updated js

--- shufflem-axios.js
+++ shufflem-axios.js
@@ -1,19 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
   
-    const baseAPI = "https://deckofcardsapi.com/api/deck";
-    let deckId = null;
+    const baseURL = "https://deckofcardsapi.com/api/deck";
+    
     let draw = $("#draw");
     let cards = $("#cards");
+    var deckId = null
 
-    draw.on('click', drawCard)
+    const shuffle = async () => {
 
-    async function drawCard() {
-        try {
-            let res = await axios.get(`${baseAPI}/new/draw/?count=1`);
-            console.log(res);
-        } catch (e) {
-            console.log("Sorry, an error has occured");
-        }
+      let newDeck = await $.getJSON(`${baseURL}/new/shuffle/`);
+      deckId = newDeck.deck_id
+        draw.show().on("click", async function () {
+        let cardData = await $.getJSON(`${baseURL}/${newDeck.deck_id}/draw/`);
+        let cardSrc = cardData.cards[0].image;
+        let angle = Math.random() * 90 - 45;
+        let randomX = Math.random() * 40 - 20;
+        let randomY = Math.random() * 40 - 20;
+        cards.append(
+          $("<img>", {
+            src: cardSrc,
+            css: {
+              transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`,
+            },
+          })
+        );
+        if (cardData.remaining === 0) draw.remove();
+      });
     }
-    
+    draw.on("click", shuffle);
 });
+
