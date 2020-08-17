43523d1acab7afba39f6d70d1c27c664f1c89898
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-08-16 20:30:06 -0700

    added DOM variables; html cards container; drawCard function

---
+++ shufflem-axios.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  
+    const baseAPI = "https://deckofcardsapi.com/api/deck";
+    let deckId = null;
+    let draw = $("#draw");
+    let cards = $("#cards");
+
+    async function drawCard() {
+        try {
+            let res = await axios.get(`${baseAPI}/new/draw/?count=1`);
+            console.log(res);
+        } catch (e) {
+            console.log("Sorry, an error has occured");
+        }
+    }
+
+drawCard();
+});
